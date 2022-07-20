bbfeb4caad9e3908035a0a10999d21ee1d550709
Anderson Pires <andersonpires855@gmail.com>
2022-07-20 15:53:58 -0300

    Solução do desafio 3 de arrays

---
+++ src/br/com/dio/arrays/Ex3_NumerosAleatorios.java
@@ -0,0 +1,27 @@
+package br.com.dio.arrays;
+
+import java.util.Random;
+
+
+public class Ex3_NumerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length ; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.print("Numeros Aleatorios: ");
+        for (int n : numerosAleatorios) {
+            System.out.print(n + " ");
+        }
+
+        System.out.print("\nSucessores dos Numeros Aleatorios: ");
+        for (int n : numerosAleatorios) {
+            System.out.print(n+1 + " ");
+        }
+    }
+}
