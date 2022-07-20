5f2ca08390e1483b7128b997910ec25612cad636
Anderson Pires <andersonpires855@gmail.com>
2022-07-20 13:59:26 -0300

    Solução do desafio 4 de loop

---
+++ src/br/com/dio/loops/Ex4_ParEImpar.java
@@ -0,0 +1,34 @@
+package br.com.dio.loops;
+
+import java.util.Scanner;
+
+public class Ex4_ParEImpar {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0, quantImpares = 0;
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+        int count = 0;
+        do {
+            System.out.println("número: ");
+            numero = scan.nextInt();
+
+            if ((numero % 2 == 0)) {
+                quantPares++;
+            } else {
+                quantImpares++;
+            }
+
+            count++;
+        }while (count < quantNumeros);
+
+        System.out.println("Quantidade de numeros pares =  " + quantPares);
+        System.out.println("Quantidade de numeros impares =  " + quantImpares);
+    }
+}
